d285142ea8d94921275dec06864e244769793ea5
Tanush Mahajan <tanush.mohina@gmail.com>
2022-11-19 19:43:22 +0530

    Second Commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div>Hi, I am Nitish Mahajan! Hehe :D </div>
+        <div className="text-white font-bold">Hi, I am Nitish Mahajan! Hehe :D </div>
       </main>
     </>
   );
